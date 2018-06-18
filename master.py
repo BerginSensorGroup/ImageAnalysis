6f4e2840990fa40c11eacfee8ada2a407c8aa221
xavierboudreau <xavi.boudreau@gmail.com>
2018-06-18 14:09:21 -0400

    Update master.py

--- master.py
+++ master.py
@@ -1,10 +1,5 @@
 import PYemail
 import camera_controller
-<<<<<<< HEAD
-import threading
-
-def camera_run(camera, save_folder, picture_number_file):
-=======
 from picamera import PiCamera
 import threading
 import datetime
@@ -12,7 +7,6 @@ from time import sleep
 import os
 
 def camera_run(camera, save_folder, stamp_folder, picture_number_file):
->>>>>>> b05acd6ce0a5b80aacbe25a35693791a0e5e0238
     '''
     Takes a picture every 60 seconds
     
@@ -23,14 +17,6 @@ def camera_run(camera, save_folder, stamp_folder, picture_number_file):
         will ever have the same name. A file is used instead of a variable so
         the number is maintained in the case of power loss
     '''
-<<<<<<< HEAD
-    camera_controller.takePicture(camera, save_folder, picture_number_file)
-    t = threading.Timer(60.0, camera_run, camera, save_folder, picture_number_file)
-    t.daemon = True #finish when main finishes
-    t.start()
-
-def sending_run(username, password, receiver, send_folder):
-=======
     camera_controller.takePicture(camera, save_folder, stamp_folder, picture_number_file)
     
     t = threading.Timer(60.0, camera_run, (camera, save_folder, stamp_folder, picture_number_file))
@@ -38,7 +24,6 @@ def sending_run(username, password, receiver, send_folder):
     t.start()
 
 def sending_run(username, password, receiver, send_folder, stamp_folder):
->>>>>>> b05acd6ce0a5b80aacbe25a35693791a0e5e0238
     '''
     Attempts to send all the files in send_folder via email
     If successful function repeats after 60 seconds
@@ -53,29 +38,13 @@ def sending_run(username, password, receiver, send_folder, stamp_folder):
 
     to_send = os.listdir(send_folder)
     to_send = [send_folder + file_name for file_name in to_send]
-<<<<<<< HEAD
-=======
     stamp_paths = os.listdir(stamp_folder)
     stamp_paths = [stamp_folder + file_name for file_name in stamp_paths]
     
->>>>>>> b05acd6ce0a5b80aacbe25a35693791a0e5e0238
     seconds_until_next_call = 0
     if PYemail.have_internet():
         server = PYemail.setupSMTP(host_address, port_number, username, password)
         if server != None:
-<<<<<<< HEAD
-            sent_all = PYemail.sendAll(username, receiver, to_send, server)
-            if sent_all:
-                seconds_until_next_call = 60
-            else:
-                seconds_until_next_call = 30
-            server.quit()
-        else:
-            seconds_until_next_call = 30
-    else:
-        seconds_until_next_call = 30
-    t = threading.Timer(60.0, sending_run, username, receiver)
-=======
             sent_all = PYemail.sendAll(username, receiver, to_send, stamp_paths, server)
             if sent_all:
                 seconds_until_next_call = 60
@@ -91,25 +60,17 @@ def sending_run(username, password, receiver, send_folder, stamp_folder):
         seconds_until_next_call = 30
         #Didn't send any: did not have internet
     t = threading.Timer(seconds_until_next_call, sending_run, (username, password, receiver, send_folder, stamp_folder))
->>>>>>> b05acd6ce0a5b80aacbe25a35693791a0e5e0238
     t.daemon = True #finish when main finishes
     t.start()
         
 if __name__ == '__main__':
     ##CONSTANTS
     
-<<<<<<< HEAD
-    save_folder = '/home/pi/Documents/facial_detection/unsent_pictures'
-    picture_number_file = '/home/pi/Documents/facial_detection/current_picture_number.txt'
-    
-    credentials_path = "berginSenderCredentials.json"
-=======
     save_folder = '/home/pi/Documents/facial_detection/unsent_pictures/'
     stamp_folder = '/home/pi/Documents/facial_detection/unsent_stamps/'
     picture_number_file = '/home/pi/Documents/facial_detection/current_picture_number.txt'
     
     credentials_path = "../Credentials/berginSenderCredentials.json"
->>>>>>> b05acd6ce0a5b80aacbe25a35693791a0e5e0238
     receiver = 'BerginReciever@gmail.com'
     #GMAIL TO GENERAL EXCHANGE (SMTP TLS)
     host_address = "smtp.gmail.com"
@@ -121,15 +82,6 @@ if __name__ == '__main__':
     
     username, password, credentials_OK = PYemail.getCredentials(credentials_path)
     
-<<<<<<< HEAD
-    camera_run(camera, save_folder, picture_number_file)
-    
-    if credentials_OK:
-        sending_run(username, password, receiver, save_folder)
-    
-    while True:
-        pass
-=======
     #wait 15 seconds on startup for wifi to connect
     if not PYemail.have_internet():
         sleep(15)
@@ -142,4 +94,3 @@ if __name__ == '__main__':
     while True:
         pass
 
->>>>>>> b05acd6ce0a5b80aacbe25a35693791a0e5e0238
