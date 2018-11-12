9254d5c27b9710c508a6f3bee03e677800a3bce7
Joshua France <joshua.france@duke.edu>
2018-11-12 18:15:12 -0500

    updated Launcher.sh file path for new pi

--- launcher.sh
+++ launcher.sh
@@ -3,7 +3,7 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/Documents/facial_detection
+cd home/pi/ImageAnalysis
 sudo python3 master.py
 sudo python3 on_termination.py
 cd /
